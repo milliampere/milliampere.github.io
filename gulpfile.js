35b9abc681cedfa9b695615f7b0fabc2c546d799
milliampere <millla@hotmail.com>
2017-03-14 16:54:13 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const postcss = require('gulp-postcss');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 
 
@@ -17,7 +16,6 @@ gulp.task('default', ['sass'], () => {
     gulp.watch("./*.html").on('change', browserSync.reload);
 
     gulp.watch('./src/scss/**/*.scss', ['sass']);
-    gulp.watch('./src/js/*.js', ['babel']);
 });
 
 
@@ -25,16 +23,7 @@ gulp.task('sass', () => {
     return gulp.src('./src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer({browsers: ['last 2 versions']}), cssnano()]))
-        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
-
-gulp.task('babel', () => {
-    return gulp.src('./src/js/*.js')
-        .pipe(babel({
-            presets: ['latest']
-        }))
-        .pipe(gulp.dest('dist/js'));
-});
